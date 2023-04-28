ecb990e73097041c4fe7914d951ec54773f1cc9b
Anthony <anthony.villegas@nodel.co>
2023-04-28 14:09:42 -0500

    confetti update

--- src/app/inapps/roulette/roulette.component.ts
+++ src/app/inapps/roulette/roulette.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
+import confetti from 'canvas-confetti';
+
 // Counter clockwise
 const symbolSegments: any = {
   1: '¡Felicidades tienes un gift card de $5  CLUBPYCCA!  Recíbelo en el stand.',
@@ -30,19 +32,35 @@ export class RouletteComponent implements OnInit {
 
     const handleWin = (actualDeg: any) => {
       const winningSymbolNr: number = Math.ceil(actualDeg / zoneSize);
-      Swal.fire({
-        imageUrl: '/assets/img/logo_tada.png',
-        width: '21em',
-        imageWidth: '12rem',
-        imageHeight: '5rem',
-        imageAlt: 'Custom image',
-        html: `<h3 style='font-family: Bright !important;font-size: 2rem;color: #270a45;'>
-              ${symbolSegments[winningSymbolNr]} 
-              </h3>`,
-        showCloseButton: false,
-        showConfirmButton: false,
-        allowOutsideClick: false,
+      // Configuramos los colores que queremos que tenga el confetti
+      const colors = ['#FF0000', '#00FF00', '#0000FF'];
+      confetti({
+        particleCount: 100, // Número de partículas
+        spread: 100, // Área en la que se distribuyen las partículas
+        origin: { y: 0.6 }, // Posición de inicio
+        colors: colors, // Colores del confetti
       });
+
+      setTimeout(() => {
+        Swal.fire({
+          imageUrl: '/assets/img/logo_tada.png',
+          width: '21em',
+          imageWidth: '12rem',
+          imageHeight: '5rem',
+          imageAlt: 'Custom image',
+          html: `<h3 style='font-family: Bright !important;font-size: 2rem;color: #270a45;'>
+                ${symbolSegments[winningSymbolNr]} 
+                </h3>`,
+          showCloseButton: false,
+          showConfirmButton: true,
+          confirmButtonText: 'Volver a jugar',
+          confirmButtonColor: '#270a45',
+          allowOutsideClick: false,
+        }).then((result) => {
+          startButton.style.pointerEvents = 'auto';
+          wheel.style.transform = `rotate(0deg)`;
+        });
+      }, 500);
     };
 
     startButton?.addEventListener('click', () => {
@@ -52,12 +70,17 @@ export class RouletteComponent implements OnInit {
       // Calculate a new rotation between 5000 and 10 000
       deg = Math.floor(3000 + Math.random() * 3000);
       let actualDeg = deg % 360;
-      while (actualDeg >= 135 && actualDeg <= 182) {
+      while (
+        (actualDeg >= 135 && actualDeg <= 182) ||
+        (actualDeg >= 0 && actualDeg <= 45) ||
+        (actualDeg >= 180 && actualDeg <= 225)
+      ) {
+        // console.log("picca")
         deg = Math.floor(3000 + Math.random() * 3000);
         actualDeg = deg % 360;
       }
-      console.log(deg);
-      console.log(actualDeg);
+      // console.log(deg);
+      // console.log(actualDeg);
       // Set the transition on the wheel
       wheel.style.transition = 'all 5s ease-out';
       // Rotate the wheel
